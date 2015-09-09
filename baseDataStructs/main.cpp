b769e2fe9d03eff751021632185c2485c9ff8fae
Sam Kreter <samkreter@gmail.com>
2015-09-09 16:37:42 -0500

    Initial Commit

---
+++ baseDataStructs/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  baseDataStructs
+//
+//  Created by Sam Kreter on 9/9/15.
+//  Copyright (c) 2015 Sam Kreter. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
